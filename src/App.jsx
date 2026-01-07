a8f8ccd7e509f42b0f928c6be4d73270df784f80
Gladis <gladyskankwala03@gmail.com>
2026-01-07 16:54:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./component/Navbar"
+
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+function Navbar() {
+    return(
+        <header className="p-5 bg-pink-600">
+            <nav>
+                <h1 className="text-2xl text-white font-extrabold">
+                    PDF Converter
+                </h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
